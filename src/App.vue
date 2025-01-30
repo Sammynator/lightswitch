0049b14dff9b74a88d095a550af84a9f01c83428
Sammynator <sgerules@gmail.com>
2025-01-30 17:19:00 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const isOn = ref(false);
+
+function toggleClass() {
+  isOn.value = !isOn.value;
+}
+</script>
+
+<template>
+  <div class="main">
+    <div class="toggle">
+      <h1 :class="{ on: isOn }">
+        Turn me {{ isOn ? "off" : "on" }}
+        <!-- Change text based on color -->
+      </h1>
+      <button @click="toggleClass"></button>
+    </div>
+  </div>
+</template>
